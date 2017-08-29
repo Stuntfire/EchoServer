34c129580833e0d2001cb2e8cb06cc66ca4d48c4
Søren Roed Schack <soer622t@edu.easj.dk>
2017-08-29 12:32:09 +0200

    Oprettet en ny tcplistener.

---
+++ EchoServer/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    class Server
+    {
+        public void Start()
+        {
+            TcpListener myTpcListener = new TcpListener(IPAddress.Loopback, 7);
+
+            myTpcListener.Start();
+        }
+    }
+}
